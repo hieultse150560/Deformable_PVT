1f41fefc465804223139a24776096122b5506bb6
Hieu Trung Le <62134254+hieultse150560@users.noreply.github.com>
2022-09-27 10:18:52 +0700

    Update dpt.py

--- dpt.py
+++ dpt.py
@@ -15,6 +15,53 @@ __all__ = [
     'dpt_tiny', 'dpt_small', 'dpt_medium'
 ]
 
+def softmax(data):
+    for i in range(data.shape[0]):
+        f = data[i,:].reshape (data.shape[1])
+        data[i,:] = torch.exp(f) / torch.sum(torch.exp(f))
+    return data
+
+class SpatialSoftmax3D(torch.nn.Module):
+    def __init__(self, height, width, depth, channel, lim=[0., 1., 0., 1., 0., 1.], temperature=None, data_format='NCHWD'):
+        super(SpatialSoftmax3D, self).__init__()
+        self.data_format = data_format
+        self.height = height
+        self.width = width
+        self.depth = depth
+        self.channel = channel
+        if temperature:
+            self.temperature = Parameter(torch.ones(1) * temperature)
+        else:
+            self.temperature = 1.
+        pos_y, pos_x, pos_z = np.meshgrid(
+            np.linspace(lim[0], lim[1], self.width),
+            np.linspace(lim[2], lim[3], self.height),
+            np.linspace(lim[4], lim[5], self.depth))
+        pos_x = torch.from_numpy(pos_x.reshape(self.height * self.width * self.depth)).float()
+        pos_y = torch.from_numpy(pos_y.reshape(self.height * self.width * self.depth)).float()
+        pos_z = torch.from_numpy(pos_z.reshape(self.height * self.width * self.depth)).float()
+        self.register_buffer('pos_x', pos_x)
+        self.register_buffer('pos_y', pos_y)
+        self.register_buffer('pos_z', pos_z)
+    def forward(self, feature):
+        # Output:
+        #   (N, C*2) x_0 y_0 ...
+        if self.data_format == 'NHWDC':
+            feature = feature.transpose(1, 4).tranpose(2, 4).tranpose(3,4).reshape(-1, self.height * self.width * self.depth)
+        else:
+            feature = feature.reshape(-1, self.height * self.width * self.depth)
+        softmax_attention = feature
+        # softmax_attention = F.softmax(feature / self.temperature, dim=-1)
+        heatmap = softmax_attention.reshape(-1, self.channel, self.height, self.width, self.depth)
+
+        eps = 1e-6
+        expected_x = torch.sum(self.pos_x * softmax_attention, dim=1, keepdim=True)/(torch.sum(softmax_attention, dim=1, keepdim=True) + eps)
+        expected_y = torch.sum(self.pos_y * softmax_attention, dim=1, keepdim=True)/(torch.sum(softmax_attention, dim=1, keepdim=True) + eps)
+        expected_z = torch.sum(self.pos_z * softmax_attention, dim=1, keepdim=True)/(torch.sum(softmax_attention, dim=1, keepdim=True) + eps)
+        expected_xyz = torch.cat([expected_x, expected_y, expected_z], 1)
+        feature_keypoints = expected_xyz.reshape(-1, self.channel, 3)
+        return feature_keypoints, heatmap
+
 
 class Mlp(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
